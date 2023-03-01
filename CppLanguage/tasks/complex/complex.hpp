e459225aa61c49d31869317e2215b23f37688372
Katya Samsonova <89536093+samsonovak8@users.noreply.github.com>
2023-03-01 18:45:03 +0300

    update complex.hpp

--- CppLanguage/tasks/complex/complex.hpp
+++ CppLanguage/tasks/complex/complex.hpp
@@ -1,3 +1,28 @@
-#pragma once
+#pragma once 
 
-class Complex {};
+class Complex {
+ public:
+    Complex() = default;
+    Complex(double re_, double im_ = 0.0);
+
+    double GetRe() const;
+    double GetIm() const;
+
+    Complex operator+();
+    Complex operator-();
+
+    Complex& operator+=(const Complex& b);
+    Complex& operator-=(const Complex& b);
+    Complex& operator*=(const Complex& b);
+
+ private:
+    double re = 0.0;
+    double im = 0.0;
+};
+
+Complex operator+(const Complex& a, const Complex& b);
+Complex operator-(const Complex& a, const Complex& b);
+Complex operator*(const Complex& a, const Complex& b);
+
+bool operator==(const Complex& a, const Complex& b);
+bool operator!=(const Complex& a, const Complex& b);

a7c46d0652e00b05cee6745bc6ebc625e0ed1f83
Katya Samsonova <89536093+samsonovak8@users.noreply.github.com>
2023-03-01 18:43:39 +0300

    Update abs.hpp

--- CppLanguage/tasks/complex/abs.hpp
+++ CppLanguage/tasks/complex/abs.hpp
@@ -1,9 +1,7 @@
-
 #include "complex.hpp"
 #include <cmath>
 
-/*
-???  Abs(??? number) {
-  return number > 0 ? number : -number;
-}
-*/
+template <typename T>
+T Abs(const T& obj);
+
+double Abs(const Complex& a);

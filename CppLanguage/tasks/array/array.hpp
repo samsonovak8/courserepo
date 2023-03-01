b8b6e9c6e0a2b4ce0755fa7252c910d795be6dee
Katya Samsonova <89536093+samsonovak8@users.noreply.github.com>
2023-03-01 18:42:11 +0300

    update array.hpp

--- CppLanguage/tasks/array/array.hpp
+++ CppLanguage/tasks/array/array.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <stdexcept>
 
 template <typename T, int N>
@@ -9,19 +10,53 @@ class Array {
     std::copy(lst.begin(), lst.end(), arr_);
   }
 
-  T& Front();
+  T& Front() {
+    return arr_[0];
+  }
 
-  T& Back();
+  const T& Front() const {
+    return arr_[0];
+  }
 
-  int Size();
+  T& Back() {
+    return arr_[N - 1];
+  }
 
-  bool Empty();
+  const T& Back() const {
+    return arr_[N - 1];
+  }
 
-  T* Data();
+  int Size() const {
+    return N;
+  }
 
-  T& operator[](int i);
+  bool Empty() const {
+    return N == 0;
+  }
 
-  void Swap(Array<T, N>& cur);
+  T* Data() {
+    return arr_;
+  }
+
+  const T* Data() const {
+    return arr_;
+  }
+
+  T& operator[](int i) {
+    return arr_[i];
+  }
+
+  const T& operator[](int i) const {
+    return arr_[i];
+  }
+
+  void Swap(Array<T, N>& cur) {
+    for(size_t i = 0; i < N; ++i) {
+      T tmp = arr_[i];
+      arr_[i] = cur.arr_[i];
+      cur.arr_[i] = tmp;
+    }
+  }
 
  private:
   T arr_[N];
